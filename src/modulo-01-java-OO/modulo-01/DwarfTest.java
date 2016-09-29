4b4c50c03ebf304d1387b53f6e3da3ac94ae74b9
Régis Martiny <regismartiny@gmail.com>
2016-09-28 23:11:52 -0300

    test(DwarfTest): Testes da classe.

--- src/modulo-01-java-OO/modulo-01/DwarfTest.java
+++ src/modulo-01-java-OO/modulo-01/DwarfTest.java
@@ -90,4 +90,43 @@ public class DwarfTest
         // Assert
         assertEquals(0, dwarf.getVida());
     }
+    
+    @Test
+    public void dwarfNascidoDia1Mes2Ano3000() {
+        // Act
+        Dwarf dwarf = new Dwarf("Frodo", new DataTerceiraEra(1,2,3000));
+        // Assert
+        assertEquals(1, dwarf.getDataNascimento().getDia());
+        assertEquals(2, dwarf.getDataNascimento().getMes());
+        assertEquals(3000, dwarf.getDataNascimento().getAno());
+    }
+    
+    public void dwarfNascidoDia10Mes12Ano4000() {
+        // Act
+        Dwarf dwarf = new Dwarf("Frodo", new DataTerceiraEra(1,2,4000));
+        // Assert
+        assertEquals(10, dwarf.getDataNascimento().getDia());
+        assertEquals(12, dwarf.getDataNascimento().getMes());
+        assertEquals(4000, dwarf.getDataNascimento().getAno());
+    }
+    
+    public void dwarfComNomeNascidoDia15Mes11Ano3000() {
+        // Act
+        Dwarf dwarf = new Dwarf("Frodo", new DataTerceiraEra(15,11,3000));
+        // Assert
+        assertEquals("Frodo", dwarf.getNome());
+        assertEquals(15, dwarf.getDataNascimento().getDia());
+        assertEquals(11, dwarf.getDataNascimento().getMes());
+        assertEquals(3000, dwarf.getDataNascimento().getAno());
+    }
+    
+    public void dwarfComNomeNascidoDia10Mes2Ano3201() {
+        // Act
+        Dwarf dwarf = new Dwarf("Anão", new DataTerceiraEra(10,2,3201));
+        // Assert
+        assertEquals("Anão", dwarf.getNome());
+        assertEquals(10, dwarf.getDataNascimento().getDia());
+        assertEquals(2, dwarf.getDataNascimento().getMes());
+        assertEquals(3201, dwarf.getDataNascimento().getAno());
+    }
 }
