1d32d30715040459f4e53c79dfc891d01083b381
Régis Martiny <regismartiny@gmail.com>
2016-09-28 22:39:24 -0300

    feat(DataTerceiraEra): Ex 3.1. Classe nova com atributos getters.

---
+++ src/modulo-01-java-OO/modulo-01/DataTerceiraEra.java
@@ -0,0 +1,20 @@
+public class DataTerceiraEra
+{
+    private int dia, mes, ano;
+    
+    public DataTerceiraEra()
+    {
+    }
+    
+    public int getDia() {
+        return dia;
+    }
+    
+    public int getMes() {
+        return mes;
+    }
+    
+    public int getAno() {
+        return ano;
+    }    
+}
