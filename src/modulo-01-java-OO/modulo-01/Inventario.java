6cbe7d99a0c26a09fb5f00aea04839304702439c
Unknown <regismartiny@gmail.com>
2016-10-03 17:12:57 -0300

    refactor(Inventario): refatorar método de ordenação de itens.

--- src/modulo-01-java-OO/modulo-01/Inventario.java
+++ src/modulo-01-java-OO/modulo-01/Inventario.java
@@ -2,106 +2,94 @@ import java.util.ArrayList;
 
 public class Inventario
 {
-   private ArrayList<Item> lista;
-   
-   public Inventario() {
-       lista = new ArrayList<>();
-   }
-    
-   public void adicionarItem(Item item) {
-       lista.add(item);
-   }
-   
-   public void removerItem(Item item) {
-       lista.remove(item);
-   }
-   
-   public String getDescricoesItens() {
-       String resultado = "";
-       for(int i=0; i < lista.size(); i++){
-           if (i>0) resultado += ",";
-           resultado += lista.get(i).getDescricao(); 
-       }
-       return resultado;
-   }
-   
-   public ArrayList<Item> getLista() {
-       return lista;
-   }
-   
-   public Item getItemMaisPopular() {
-       Item itemMaisPopular = null;
-       int maiorQuantidade = 0;
-       for(Item i: lista){
-           int quant = i.getQuantidade();
-           if(quant > maiorQuantidade){
-               maiorQuantidade = quant;
-               itemMaisPopular = i;
-           }
-       }
-       return itemMaisPopular;
-   }
-   
-   public void aumentar1000UnidadesDosItens() {
-       for(Item i: lista){
-           i.setQuantidade(i.getQuantidade()+1000);
-       }
-   }
-   
-   public void ordenarItens() {
-       boolean troca = true;
-       while(troca) {
-           troca = false;
-           for(int i=0; i < lista.size() - 1; i++) {
-               if (lista.get(i).getQuantidade() > lista.get(i+1).getQuantidade()) {
-                   Item tmp = lista.get(i);
-                   
-                   lista.set(i, lista.get(i+1));
-                   lista.set(i+1, tmp);
-                   troca = true;
-               }
-           }
-       }
-   }
-   
-   public void ordenarItensDesc() {
-       boolean troca = true;
-       while(troca) {
-           troca = false;
-           for(int i=lista.size()-1; i > 0; i--) {
-               if (lista.get(i).getQuantidade() > lista.get(i-1).getQuantidade()) {
-                   Item tmp = lista.get(i);
-                   lista.set(i, lista.get(i-1));
-                   lista.set(i-1, tmp);
-                   troca = true;
-               }
-           }
-       }
-   }
-   
-   public void aumentar1000VezesASomaUnidadesDosItens() {
-       for (Item i: lista) {
-           int quant = i.getQuantidade();
-           int quantParaSoma = quant < 0? quant * -1: quant;
-           int soma = getSoma(quantParaSoma);
-           i.setQuantidade(quant + 1000 * soma);
-       }
-   }
-   
-   private int getSoma(int num) {
-       if (num <= 0) {
+    private ArrayList<Item> lista;
+
+    public Inventario() {
+        lista = new ArrayList<>();
+    }
+
+    public void adicionarItem(Item item) {
+        lista.add(item);
+    }
+
+    public void removerItem(Item item) {
+        lista.remove(item);
+    }
+
+    public String getDescricoesItens() {
+        String resultado = "";
+        for(int i=0; i < lista.size(); i++){
+            if (i>0) resultado += ",";
+            resultado += lista.get(i).getDescricao(); 
+        }
+        return resultado;
+    }
+
+    public ArrayList<Item> getLista() {
+        return lista;
+    }
+
+    public Item getItemMaisPopular() {
+        Item itemMaisPopular = null;
+        int maiorQuantidade = 0;
+        for(Item i: lista){
+            int quant = i.getQuantidade();
+            if(quant > maiorQuantidade){
+                maiorQuantidade = quant;
+                itemMaisPopular = i;
+            }
+        }
+        return itemMaisPopular;
+    }
+
+    public void aumentar1000UnidadesDosItens() {
+        for(Item i: lista){
+            i.setQuantidade(i.getQuantidade()+1000);
+        }
+    }
+
+    public void aumentar1000VezesASomaUnidadesDosItens() {
+        for (Item i: lista) {
+            int quant = i.getQuantidade();
+            int quantParaSoma = quant < 0? quant * -1: quant;
+            int soma = getSoma(quantParaSoma);
+            i.setQuantidade(quant + 1000 * soma);
+        }
+    }
+
+    private int getSoma(int num) {
+        if (num <= 0) {
             return 0;
         } else {
             return getSoma(num - 1) + num;
         }
-   }
-   
-   public void ordenarItens(TipoOrdenacao ord) {
-       //TO-DO: Refatorar ordenação de itens, trazer para este método
-       if(TipoOrdenacao.ASCENDENTE == ord) {
-           ordenarItens();
-       }else{
-           ordenarItensDesc();
-       }
-   }
+    }
+
+    public void ordenarItens() { 
+        ordenarItens(TipoOrdenacao.ASCENDENTE); 
+    } 
+ 
+    public void ordenarItens(TipoOrdenacao tipoOrdenacao) {
+        // Versão mais estável do Bubblesort - Melhor caso O(n), Pior caso O(n^2)
+        // homenagem ao do-while: para forçar entrada na lógica
+        boolean posicoesSendoTrocadas;
+        boolean ascendente = tipoOrdenacao == TipoOrdenacao.ASCENDENTE;
+        do {
+            posicoesSendoTrocadas = false;
+            for (int j = 0; j < this.lista.size() - 1; j++) {
+                Item itemAtual = this.lista.get(j);
+                Item proximo = this.lista.get(j + 1);
+
+                boolean precisaTrocar = 
+                    ascendente ? itemAtual.getQuantidade() > proximo.getQuantidade() : itemAtual.getQuantidade() < proximo.getQuantidade();
+
+                if (precisaTrocar) {
+                    this.lista.set(j, proximo);
+                    this.lista.set(j + 1, itemAtual);
+                    posicoesSendoTrocadas = true;
+                }
+            }
+        } while (posicoesSendoTrocadas);
+
+    }
 }
