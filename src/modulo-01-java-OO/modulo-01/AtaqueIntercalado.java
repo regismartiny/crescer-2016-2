8675a85cf4d71e27a330e87358d7b6806aec1a69
Régis Martiny <regismartiny@gmail.com>
2016-10-08 15:05:46 -0300

    refactor(AtaqueIntercalado): refatorar.

--- src/modulo-01-java-OO/modulo-01/AtaqueIntercalado.java
+++ src/modulo-01-java-OO/modulo-01/AtaqueIntercalado.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class AtaqueIntercalado implements Estrategia
 {
     public List<Elfo> getOrdemDeAtaque(List<Elfo> atacantes, List<Dwarf> alvos) throws ContingenteDesproporcionalException {
-        List lista = new ArrayList<Elfo>();
+        List<Elfo> lista = new ArrayList<>();
         int nElfosVerdes = 0, nElfosNoturnos = 0;
         for(Elfo elfo : atacantes) {
             if (Status.VIVO.equals(elfo.getStatus())) {
@@ -23,29 +23,23 @@ public class AtaqueIntercalado implements Estrategia
     }
     
     
-    private List ordernarListaTiposIntercalados(List lista) {
-        List<Object> listaIntercalada = new ArrayList<>();
+    private List<Elfo> ordernarListaTiposIntercalados(List<Elfo> lista) {
+        List<Elfo> listaIntercalada = new ArrayList<>();
         for(int i=0; i < lista.size() - 1; i++) {
-            Object atual = lista.get(i);
-            Object proximo = lista.get(i+1);
+            Elfo atual = lista.get(i);
+            Elfo proximo = lista.get(i+1);
             listaIntercalada.add(atual);
             boolean ehDoMesmoTipo = atual.getClass() == proximo.getClass();
             if (ehDoMesmoTipo) {
                 for(int j=i+2; j < lista.size(); j++) {
-                    Object proximo2 = lista.get(j);
+                    Elfo proximo2 = lista.get(j);
                     boolean aindaEhDoMesmoTipo = proximo.getClass() == proximo2.getClass();
-                    if (aindaEhDoMesmoTipo) {
-                        continue;
-                    }
-                    else {
+                    if (!aindaEhDoMesmoTipo) {
                         lista.set(i+1, proximo2);
                         lista.set(j, proximo);
                         listaIntercalada.add(proximo2);
                     }
                 }
-            }
-            else{
-                listaIntercalada.add(proximo);
             }
         }
         return listaIntercalada;
