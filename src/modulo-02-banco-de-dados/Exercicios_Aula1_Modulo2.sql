a1bedbe06ed63525c73fcdc6a19f7ff72310af1f
Unknown <regismartiny@gmail.com>
2016-10-11 14:05:41 -0300

    feat(Exercicios): exercicios aula 1 modulo 2.

---
+++ src/modulo-02-banco-de-dados/Exercicios_Aula1_Modulo2.sql
@@ -0,0 +1,24 @@
+select *
+into CidadeAux
+from Cidade
+
+truncate table CidadeAux;
+
+insert into CidadeAux (IDCidade, Nome, UF)
+select IDCidade, Nome, UF
+from Cidade;
+
+create table Produto
+(
+	IDProduto	int identity	not null,
+	Nome		varchar(20)		not null,
+	Descricao	varchar(60)		not null,
+	DataCriacao datetime		default getdate() not null,
+	LocalEstoque varchar(30)	not null,
+	Quantidade	decimal(7,3)	not null, -- valor maximo: 9999.999
+	Preco		decimal(7,2)	not null, -- valor maximo: 99999,99
+		constraint PK_Produto primary key (IDProduto)
+);
+
+insert into Produto (Nome, Descricao, LocalEstoque, Quantidade, Preco)
+	values ('Alcatra', 'Carne Bovina Alcatra', 'Açougue do Jorge', 1.0, 25.00);
