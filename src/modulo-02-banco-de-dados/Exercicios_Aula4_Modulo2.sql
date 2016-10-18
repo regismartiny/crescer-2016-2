2ff9d6983cda3e10da3de53605d0fb8f3a003a64
Unknown <regismartiny@gmail.com>
2016-10-18 17:23:40 -0200

    fix(exerciciosAula4Modulo2): correção exercicios aula 4 modulo 2.

--- src/modulo-02-banco-de-dados/Exercicios_Aula4_Modulo2.sql
+++ src/modulo-02-banco-de-dados/Exercicios_Aula4_Modulo2.sql
@@ -2,22 +2,56 @@
 
 select count(*) 
 from Pedido
-where DataPedido between convert(datetime,'01/09/2016',103) and convert(datetime,'30/09/2016',103);
+where DataPedido between convert(datetime,'01/09/2016',103) and convert(datetime,'30/09/2016',103)+.99999;
+
+-- 1.0 = 1 dia(24h)
+-- 0.99999 = 23h:59m:59s
 
 
 -- ex 2
 
-select Nome
+select IDProduto
+from ProdutoMaterial
+where IDMaterial = 15836;
+
+--ou
+
+select p.IDProduto, Nome
 from Produto p
 inner join ProdutoMaterial pm on pm.IDProduto = p.IDProduto
 where pm.IDMaterial = 15836;
 
+--ou
+
+select p.IDProduto, Nome
+from Produto p
+where exists (select 1
+			  from ProdutoMaterial pm
+			  where pm.IDProduto = p.IDproduto
+			  and pm.IDMaterial = 15836);
+
+
+
+begin transaction
+--criação de índice
+create index IX_ProdutoMaterial_Material on ProdutoMaterial (IDMaterial);
+
+--índice composto
+-- criar quando ambas as colunas são utilizadas como filtro
+selec IDprodutoMaterial, IDProduto
+from ProdutoMaterial PM
+where pm.IDMateria = 15836
+and pm.IDproduto
+
+create index IX_ProdutoMaterial_MaterialProduto on ProdutoMaterial (IDMaterial,IDProduto);
+
+rollback
 
 -- ex 3
 
 select IDCliente, Nome, RazaoSocial
 from Cliente
-where Nome like '%LTDA' or RazaoSocial like '%LTDA';
+where Nome like '%LTDA%' or RazaoSocial like '%LTDA%';
 
 
 -- ex 4
@@ -29,12 +63,25 @@ insert
 
 -- ex 5
 
-select distinct Nome as Produto_Sem_Pedidos
+select Nome as Produto_Sem_Pedidos
 	from Produto
 	where IDProduto not in (select p.IDProduto
 								from Produto p
 								inner join PedidoItem pei on p.IDProduto = pei.IDproduto
 								inner join Pedido pe on pei.IDPedido = pe.IDPedido);
+
+--ou
+
+select IDProduto, Nome
+From Produto pro
+Where IDproduto not in (Select IDproduto From PedidoItem item);
+
+--com not exists     mais performatico(principalmente se criar indice)
+select Nome as Produto_Sem_Pedidos
+	from Produto p
+	where not exists (select 1
+					  from PedidoItem i
+					  where p.IDProduto = i.IDproduto);
 
 
 -- ex 6
@@ -67,6 +114,7 @@ select p.IDPedido, p.DataEntrega, p.ValorPedido, (select count(1) from PedidoIte
 							and convert(datetime,'30/10/2016',103);
 
 
--- ex 8
+
+
 
 
