c3123d92e0ef64d137641eae0b3410c4befa0b09
Unknown <regismartiny@gmail.com>
2016-10-29 12:10:35 -0200

    feat(exC#MarioKart): nivel 3. Em andamento...

---
+++ src/modulo-05-c#/ExercicioMarioKart/ExercicioMarioKart/Equipamentos/FogueteDePlutonio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioMarioKart
+{
+    class FogueteDePlutonio : IEquipamentos
+    {
+        public FogueteDePlutonio(Nivel nivel = Nivel.UM)
+        {
+            this.Nivel = nivel;
+        }
+        public Nivel Nivel { get; set; }
+        public int Bonus
+        {
+            get
+            {
+                return (int)this.Nivel + 1;
+            }
+        }
+    }
+}
---
+++ src/modulo-05-c#/ExercicioMarioKart/ExercicioMarioKart/Equipamentos/UltraPack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioMarioKart.Equipamentos
+{
+    class UltraPack : IEquipamentos
+    {
+        public UltraPack(IEquipamentos equipamentoInterno)
+        {
+            this.EquipamentoInterno = equipamentoInterno;
+        }
+        public IEquipamentos EquipamentoInterno { get; set; }
+        public int Bonus
+        {
+            get
+            {
+                return (int)(this.EquipamentoInterno.Bonus * 1.2);
+            }
+        }
+    }
+}
