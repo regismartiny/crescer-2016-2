b4931f3d9813ba2dbaba36cdc570dd7b0b76e77b
regis.martiny <regismartiny@gmail.com>
2016-10-31 17:04:29 -0200

    feat(exStreetFighterMVC): converter site street fighter para MVC.

---
+++ src/modulo-05-dot-net/Meus arquivos/StreetFighter.Web/StreetFighter.Web/Controllers/StreetFighterController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreetFighter.Web.Controllers
+{
+    public class StreetFighterController : Controller
+    {
+        // GET: StreetFighter
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    public ActionResult FichaTecnica()
+    {
+        var model = new FichaTecnicaModel();
+
+        model.PrimeiraAparicao = "";
+
+        return ViewContext(model);
+    }
+}
